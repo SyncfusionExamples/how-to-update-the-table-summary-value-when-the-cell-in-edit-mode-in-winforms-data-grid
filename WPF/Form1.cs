03faeed04374a2506c90fa0d43c0c47131170b12
Dhanasekar Mohanraj <dhanasekar.mohanraj@syncfusion.com>
2020-11-02 16:15:33 +0530

    How to update the table summary value when the cell in edit mode in WinForms DataGrid (SfDataGrid) ?

---
+++ WPF/Form1.cs
@@ -0,0 +1,100 @@
+﻿using Syncfusion.Data;
+using Syncfusion.WinForms.DataGrid;
+using Syncfusion.WinForms.DataGrid.Enums;
+using Syncfusion.WinForms.DataGrid.Renderers;
+using Syncfusion.WinForms.DataGrid.Styles;
+using Syncfusion.WinForms.GridCommon.ScrollAxis;
+using Syncfusion.WinForms.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Update_Live
+{
+    public partial class Form1 : Form
+    {
+        SfDataGrid sfDataGrid;
+        OrderInfoCollection order;
+        OrderInfo orderInfo;
+        public Form1()
+        {
+            InitializeComponent();
+            sfDataGrid = new SfDataGrid();
+            order = new OrderInfoCollection();
+            sfDataGrid.Dock = DockStyle.Fill;
+            this.Controls.Add(sfDataGrid);
+            sfDataGrid.ShowGroupDropArea = true;
+            sfDataGrid.AllowGrouping = true;
+            sfDataGrid.DataSource = order.Orders;
+            GridTableSummaryRow tableSummaryRow1 = new GridTableSummaryRow();
+            tableSummaryRow1.Name = "TableSummary";
+            tableSummaryRow1.ShowSummaryInRow = false;
+            tableSummaryRow1.Position = VerticalPosition.Bottom;
+            GridSummaryColumn summaryColumn1 = new GridSummaryColumn();
+            summaryColumn1.Name = "PriceAmount";
+            summaryColumn1.Format = "{Sum:c}";
+            summaryColumn1.MappingName = "UnitPrice";
+            summaryColumn1.SummaryType = SummaryType.DoubleAggregate;
+            tableSummaryRow1.SummaryColumns.Add(summaryColumn1);
+            this.sfDataGrid.TableSummaryRows.Add(tableSummaryRow1);
+            this.sfDataGrid.LiveDataUpdateMode = LiveDataUpdateMode.AllowSummaryUpdate;
+            this.sfDataGrid.CellRenderers.Remove("Numeric");
+            this.sfDataGrid.CellRenderers.Add("Numeric", new GridNumericCellRendererExt(this.sfDataGrid));
+        }
+    }
+
+
+
+    public class GridNumericCellRendererExt : GridNumericCellRenderer
+    {
+        RowColumnIndex RowColumnIndex { get; set; }
+        SfDataGrid DataGrid { get; set; }
+        public GridNumericCellRendererExt(SfDataGrid dataGrid)
+        {
+            this.DataGrid = dataGrid;
+        }
+        protected override void OnInitializeEditElement(DataColumnBase column, RowColumnIndex rowColumnIndex, SfNumericTextBox uiElement)
+        {
+            base.OnInitializeEditElement(column, rowColumnIndex, uiElement);
+            uiElement.TextChanged += UiElement_TextChanged;
+
+            this.RowColumnIndex = rowColumnIndex;
+        }
+
+        private void UiElement_TextChanged(object sender, EventArgs e)
+        {
+            UpdateSummaryValues(this.RowColumnIndex.RowIndex, this.RowColumnIndex.ColumnIndex);
+        }
+
+        private void UpdateSummaryValues(int rowIndex, int columnIndex)
+        {
+            string editEelementText = string.IsNullOrEmpty(this.CurrentCellRendererElement.Text) ? "0" : this.CurrentCellRendererElement.Text;
+            columnIndex = this.TableControl.ResolveToGridVisibleColumnIndex(columnIndex);
+            if (columnIndex < 0)
+                return;
+            var mappingName = DataGrid.Columns[columnIndex].MappingName;
+            var recordIndex = this.TableControl.ResolveToRecordIndex(rowIndex);
+            if (recordIndex < 0)
+                return;
+            if (DataGrid.View.TopLevelGroup != null)
+            {
+                var record = DataGrid.View.TopLevelGroup.DisplayElements[recordIndex];
+                if (!record.IsRecords)
+                    return;
+                var data = (record as RecordEntry).Data;              
+                data.GetType().GetProperty(mappingName).SetValue(data, (int.Parse(editEelementText)));
+            }
+            else
+            {
+                var record1 = DataGrid.View.Records.GetItemAt(recordIndex);
+                record1.GetType().GetProperty(mappingName).SetValue(record1, (int.Parse(editEelementText)));
+            }
+        }
+    }
+}
---
+++ WPF/OrderInfoCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Update_Live
+{
+    public class OrderInfoCollection
+    {
+        private ObservableCollection<OrderInfo> _orders;
+        public ObservableCollection<OrderInfo> Orders
+        {
+            get { return _orders; }
+            set { _orders = value; }
+        }
+        public OrderInfoCollection()
+        {
+            _orders = new ObservableCollection<OrderInfo>();
+            this.GenerateOrders();
+        }
+        private void GenerateOrders()
+        {
+            _orders.Add(new OrderInfo(1001, "Maria Anders", "Germany", "ALFKI", "Berlin", 5));
+            _orders.Add(new OrderInfo(1002, "Ana Trujilo", "Mexico", "ANATR", "Mexico D.F.",10));
+            _orders.Add(new OrderInfo(1003, "Antonio Moreno", "Mexico", "ANTON", "Mexico D.F.",5));
+            _orders.Add(new OrderInfo(1004, "Thomas Hardy", "UK", "AROUT", "London",10));
+            _orders.Add(new OrderInfo(1005, "Christina Berglund", "Sweden", "BERGS", "Lula",5));
+            _orders.Add(new OrderInfo(1006, "Hanna Moos", "Germany", "BLAUS", "Mannheim",10));
+            _orders.Add(new OrderInfo(1007, "Frederique Citeaux", "France", "BLONP", "Strasbourg",5));
+            _orders.Add(new OrderInfo(1008, "Martin Sommer", "Spain", "BOLID", "Madrid",10));
+            _orders.Add(new OrderInfo(1009, "Laurence Lebihan", "France", "BONAP", "Marseille",5));
+            _orders.Add(new OrderInfo(1010, "Elizabeth Lincoln", "Canada", "BOTTM", "Tsawassen",10));
+        }
+    }
+}
